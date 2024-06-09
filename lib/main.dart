b75cc9e5695add0918c77538f29664ee9496ee50
mo7meddgom3a <mohamedgomaa0115@gmail.com>
2024-06-09 11:52:40 +0300

    link auth with the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:anwer_shop/auth/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:anwer_shop/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
+import 'package:anwer_shop/auth/blocs/sign_up_bloc/sign_up_bloc.dart';
+import 'package:anwer_shop/auth/firebase_user_repo.dart';
+import 'package:anwer_shop/auth/screens/login_screen/login_screen.dart';
 import 'package:anwer_shop/store/cart/cubit/cart_cubit.dart';
 import 'package:anwer_shop/store/cart/view/cart_view.dart';
 import 'package:anwer_shop/store/catalog/view/catalog_view.dart';
@@ -6,7 +11,6 @@ import 'package:anwer_shop/store/product_details/views/product_details_view.dart
 import 'package:anwer_shop/store/store_items/cubit/categories_cubit.dart';
 import 'package:anwer_shop/store/store_items/cubit/store_items_cubit.dart';
 import 'package:anwer_shop/store/store_items/models/product_model.dart';
-import 'package:anwer_shop/store/store_items/views/store_view.dart';
 import 'package:anwer_shop/store/wishlist/cubit/wish_list_cubit.dart';
 import 'package:anwer_shop/store/wishlist/view/wishlist_view.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -40,6 +44,13 @@ class ShopsApp extends StatelessWidget {
         ),
         BlocProvider(
           create: (context) => StoreCategoriesCubit(),
+        ),BlocProvider(
+          create: (context) => SignInBloc(userRepository: FirebaseUserRepo()),
+        ),BlocProvider(
+          create: (context) => SignUpBloc(userRepository: FirebaseUserRepo()),
+        ),
+        BlocProvider(
+          create: (context) => AuthenticationBloc(userRepo: FirebaseUserRepo()),
         ),
       ],
       child: MaterialApp(
@@ -64,7 +75,7 @@ class ShopsApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const StoreView(),
+        home: const LoginScreen(),
       ),
     );
   }
